b83c26d5c5985c26b7f2104ea6164ccbc3e2784a
Sandra <sandrarequena1999@gmail.com>
2023-11-18 21:53:47 +0100

    Bug fk

--- src/app/create-query/create-query.component.ts
+++ src/app/create-query/create-query.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { IPerson } from '../interfaces/i-person';
 import { PersonService } from '../services/person.service';
 import { IDatabase } from '../interfaces/i-database';
@@ -16,7 +16,7 @@ export class CreateQueryComponent implements OnInit {
     name_db: "",
     tables: []
   }
-  constructor(private personService:PersonService) { }
+  constructor(private personService:PersonService, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     this.tabla_vacia = <HTMLElement> document.querySelector('fieldset');
@@ -41,12 +41,21 @@ export class CreateQueryComponent implements OnInit {
     container.appendChild(this.tabla_vacia.cloneNode(true));
     let inputs_table = container.querySelectorAll('input');
     for (let inp of Array.from(inputs_table)) {
+      if (inp.getAttribute("id")?.includes("name_table")) {
+        const range = document.createRange();
+        const fragment = range.createContextualFragment(`<input type="text" class="form-control" id="name_table${this.tablas}"
+        placeholder="Nombre de la tabla" autocomplete="off" (input)="getSelectOptions()">`);
+        inp = fragment.firstChild as HTMLInputElement;
+        console.log(inp);
+      }
       inp.setAttribute("id", inp.getAttribute("id")!+this.tablas)
       inp.value = ""
     }
     let labels_table = container.querySelectorAll('label');
     for (let lab of Array.from(labels_table)) lab.setAttribute("for", lab.getAttribute("for")!+this.tablas)
     this.tablas++;
+  console.log(container.querySelector("fieldset"));
+
     return <HTMLFieldSetElement> container.querySelector("fieldset")
   }
 
@@ -55,12 +64,25 @@ export class CreateQueryComponent implements OnInit {
       document.querySelector('div.row')?.appendChild(this.changeAttr().cloneNode(true))
   }
 
-  createQuery() {
+  getTableNames():string[] {
+    let tableNames:string[] = []
     for(let tabla of Array.from(document.querySelectorAll('fieldset'))) {
       let container = document.createElement("div")
       container.appendChild(tabla.cloneNode(true));
-      this.query.tables.push({name_table: container.getElementsByTagName('input')[0].value})
+      tableNames.push(container.getElementsByTagName('input')[0].value)
     }
+    console.log("Chorprecha");
+    return tableNames;
+  }
+
+  getSelectOptions() {
+    this.query.tables = []
+    for(let nombre of this.getTableNames()) this.query.tables.push({name_table: nombre})
+  }
+
+  createQuery() {
+    this.getSelectOptions()
     console.log(this.query);
   }
 }
+
