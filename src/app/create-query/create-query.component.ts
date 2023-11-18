fc09b5a57eba3582ba9ba00206fc21f70bcfeedc
Sandra <sandrarequena1999@gmail.com>
2023-11-18 19:44:15 +0100

    Organización de datos en json

--- src/app/create-query/create-query.component.ts
+++ src/app/create-query/create-query.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IPerson } from '../interfaces/i-person';
 import { PersonService } from '../services/person.service';
+import { IDatabase } from '../interfaces/i-database';
 
 @Component({
   selector: 'create-query',
@@ -10,6 +11,11 @@ import { PersonService } from '../services/person.service';
 export class CreateQueryComponent implements OnInit {
   persona:IPerson = this.newPerson()
   tabla_vacia!:HTMLElement
+  tablas:number = 2
+  query:IDatabase = {
+    name_db: "",
+    tables: []
+  }
   constructor(private personService:PersonService) { }
 
   ngOnInit(): void {
@@ -30,13 +36,31 @@ export class CreateQueryComponent implements OnInit {
     });
   }
 
+  changeAttr():HTMLFieldSetElement {
+    let container = document.createElement("div")
+    container.appendChild(this.tabla_vacia.cloneNode(true));
+    let inputs_table = container.querySelectorAll('input');
+    for (let inp of Array.from(inputs_table)) {
+      inp.setAttribute("id", inp.getAttribute("id")!+this.tablas)
+      inp.value = ""
+    }
+    let labels_table = container.querySelectorAll('label');
+    for (let lab of Array.from(labels_table)) lab.setAttribute("for", lab.getAttribute("for")!+this.tablas)
+    this.tablas++;
+    return <HTMLFieldSetElement> container.querySelector("fieldset")
+  }
+
   addTable(numberTables:number) {
     for(let i = 0 ; i < numberTables; i++)
-      document.querySelector('form')?.insertBefore(this.tabla_vacia.cloneNode(true), this.tabla_vacia)
+      document.querySelector('div.row')?.appendChild(this.changeAttr().cloneNode(true))
   }
 
   createQuery() {
-    console.log("En desarrollo");
-
+    for(let tabla of Array.from(document.querySelectorAll('fieldset'))) {
+      let container = document.createElement("div")
+      container.appendChild(tabla.cloneNode(true));
+      this.query.tables.push({name_table: container.getElementsByTagName('input')[0].value})
+    }
+    console.log(this.query);
   }
 }
